6c0d9b3f29f1ef919581a2609fb8498f7202e8a6
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-22 13:22:01 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,8 +1,14 @@
 // my first javascript 
+console.log("Hello Winc Academy");
 let name = 'Lukas';
 console.log(name);
-let number = (4 + 4 - 2) * 5 / 2;
-console.log(number);
+
+let sum = (4 + 4 - 2) * 5 / 2;
+console.log(sum);
+
 let age = 37;
 console.log(age);
 console.log(typeof age);
+
+let ageString = '37';
+console.log(typeof ageString);
